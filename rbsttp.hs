79217d8fc063125bf4ebb9b1e87af1f53354d0c0
Echo Nolan <echo@echonolan.net>
2012-09-19 15:49:41 -0700

    Slightly less ghetto synthesizer

--- rbsttp.hs
+++ rbsttp.hs
@@ -3,8 +3,16 @@ import Control.Monad
 import Data.List
 import System.Process
 
-data Note = C2 | C3 | C4 | C5 | C6 deriving (Show, Enum)
+-- 12 note chromatic scale starting at middle C.
+data Note =
+    C | Csharp | D | Dsharp | E | F | Fsharp | G | Gsharp | A | Asharp | B
+    | Cagain
+    deriving (Show, Enum)
 
--- Play a note, asynchronously.
-playNote :: Note -> IO ()
-playNote n = void $ createProcess (shell $ "mplayer -really-quiet " ++ show n ++ ".ogg 2>/dev/null")
+-- Play a note with a given gain relative to max volume, asynchronously.
+playNote :: Int -> Note -> IO ()
+playNote g n = void $ createProcess $ shell $
+    "sox -n -d --no-show-progress synth 0.3 pluck " ++ show (f n) ++ " gain " ++ show g
+    where
+    f :: Note -> Double
+    f n = 261.626 * (2 ** ((fromIntegral $ fromEnum n)/12))
